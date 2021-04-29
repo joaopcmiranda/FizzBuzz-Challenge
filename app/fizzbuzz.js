7703cc7903869202a5fa819f393100e2e02b2d6e
joao <joao@streethawk.co>
2021-04-29 13:13:58 +1000

    Fizz implementation

--- app/fizzbuzz.js
+++ app/fizzbuzz.js
@@ -1,3 +1,17 @@
 module.exports = function (input) {
-    return null
+    if (input == null || isNaN(input) || input < 0) {
+        return null
+    }
+
+    let output = ""
+
+    if (input % 3 === 0) {
+        output += "Fizz"
+    }
+
+    if (output.length > 0) {
+        return output
+    } else {
+        return input.toString()
+    }
 }
