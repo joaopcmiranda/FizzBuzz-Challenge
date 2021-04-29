66844b86d8c0116da9c378dc9847d587b795257c
joao <joao@streethawk.co>
2021-04-29 12:58:25 +1000

    Updated tests to suite

--- test/fizzbuzz.js
+++ test/fizzbuzz.js
@@ -3,35 +3,58 @@ const fizzbuzz = require("../app/fizzbuzz");
 const {suite} = require('mocha');
 
 suite("Fizz Buzz", () => {
-    suite("Fizz", () => {
+    test("return should contain Fizz when multiple of 3", () => {
+        expect(fizzbuzz(0)).to.contain("Fizz")
+        expect(fizzbuzz(3)).to.contain("Fizz")
+        expect(fizzbuzz(6)).to.contain("Fizz")
+        expect(fizzbuzz(60)).to.contain("Fizz")
+        expect(fizzbuzz(123)).to.contain("Fizz")
+    })
+
+    test("return should not contain Fizz when not multiple of 3", () => {
+        expect(fizzbuzz(5)).to.not.contain("Fizz")
+        expect(fizzbuzz(50)).to.not.contain("Fizz")
+        expect(fizzbuzz(133)).to.not.contain("Fizz")
+    })
+
+    test("return should contain Buzz when multiple of 5", () => {
+        expect(fizzbuzz(0)).to.contain("Buzz")
+        expect(fizzbuzz(5)).to.contain("Buzz")
+        expect(fizzbuzz(10)).to.contain("Buzz")
+        expect(fizzbuzz(100)).to.contain("Buzz")
+        expect(fizzbuzz(1555)).to.contain("Buzz")
+    })
+
+    test("return should not contain Buzz when not multiple of 5", () => {
+        expect(fizzbuzz(3)).to.not.contain("Buzz")
+        expect(fizzbuzz(32)).to.not.contain("Buzz")
+        expect(fizzbuzz(133)).to.not.contain("Buzz")
+    })
+
+    test("return should contain Fizz when multiple of 3", () => {
+        expect(fizzbuzz(0)).to.contain("Fizz")
+        expect(fizzbuzz(3)).to.contain("Fizz")
+        expect(fizzbuzz(6)).to.contain("Fizz")
+        expect(fizzbuzz(60)).to.contain("Fizz")
+        expect(fizzbuzz(123)).to.contain("Fizz")
+    })
+
+    test("return should contain FizzBuzz only when multiple of 3 and 5", () => {
+        expect(fizzbuzz(0)).to.equal("FizzBuzz")
         expect(fizzbuzz(3)).to.equal("Fizz")
-        expect(fizzbuzz(0)).to.equal("0")
-        expect(fizzbuzz(5)).to.not.equal("Fizz")
-        expect(fizzbuzz(0)).to.not.equal("Fizz")
-        expect(fizzbuzz(6)).to.equal("Fizz")
-        expect(fizzbuzz(123)).to.equal("Fizz")
-        expect(fizzbuzz(null)).to.equal(null)
-        expect(fizzbuzz(undefined)).to.equal(undefined)
-    })
-
-    suite("Buzz", () => {
         expect(fizzbuzz(5)).to.equal("Buzz")
-        expect(fizzbuzz(1)).to.equal("1")
-        expect(fizzbuzz(3)).to.not.equal("Buzz")
-        expect(fizzbuzz(0)).to.not.equal("Buzz")
-        expect(fizzbuzz(10)).to.equal("Buzz")
-        expect(fizzbuzz(205)).to.equal("Buzz")
-        expect(fizzbuzz(null)).to.equal(null)
-        expect(fizzbuzz(undefined)).to.equal(undefined)
+        expect(fizzbuzz(15)).to.equal("FizzBuzz")
+        expect(fizzbuzz(3315)).to.equal("FizzBuzz")
+        expect(fizzbuzz(133)).to.not.equal("FizzBuzz")
     })
 
-    suite("FizzBuzz", () => {
-        expect(fizzbuzz(60)).to.equal("FizzBuzz")
-        expect(fizzbuzz(0)).to.equal("0")
-        expect(fizzbuzz(5)).to.not.equal("FizzBuzz")
-        expect(fizzbuzz(0)).to.not.equal("FizzBuzz")
-        expect(fizzbuzz(120)).to.equal("FizzBuzz")
+    test("if not a positive number should return null", () => {
         expect(fizzbuzz(null)).to.equal(null)
-        expect(fizzbuzz(undefined)).to.equal(undefined)
+        expect(fizzbuzz(undefined)).to.equal(null)
+        expect(fizzbuzz("ame")).to.equal(null)
+        expect(fizzbuzz({rep: "pe"})).to.equal(null)
+        expect(fizzbuzz({value: 2})).to.equal(null)
+        expect(fizzbuzz(-3)).to.equal(null)
     })
+
 })
